306f380848ce9d9c9618bd08c2df988d428bbb38
rymate1234 <ryan@rymate.co.uk>
2013-06-08 10:31:50 +0100

    Code formatting

--- src/de/bananaco/bpermissions/imp/ExtraCommands.java
+++ src/de/bananaco/bpermissions/imp/ExtraCommands.java
@@ -12,39 +12,39 @@ import de.bananaco.bpermissions.api.WorldManager;
 
 public class ExtraCommands {
 
-	private static WorldManager wm = WorldManager.getInstance();
-	
-	public static void execute(String name, CalculableType type, String action, String value, String world) {
-		Set<World> worlds = new HashSet<World>();
-		// add all if null
-		if(world == null)
-			worlds.addAll(wm.getAllWorlds());
-		else
-			worlds.add(wm.getWorld(world));
-		for(World w : worlds) {
-			Calculable c = w.get(name, type);
-			if(action.equalsIgnoreCase("addgroup")) {
-				c.addGroup(value);
-			} else if(action.equalsIgnoreCase("rmgroup")) {
-				c.removeGroup(value);
-			} else if(action.equalsIgnoreCase("setgroup")) {
-				for(String g : new ArrayList<String>(c.getGroupsAsString())) {
-					c.removeGroup(g);
-				}
-				c.addGroup(value);
-			} else if(action.equalsIgnoreCase("addperm")) {
-				Permission perm = Permission.loadFromString(value);
-				c.addPermission(perm.nameLowerCase(), perm.isTrue());
-			} else if(action.equalsIgnoreCase("rmperm")) {
-				c.removePermission(value);
-			} else if (action.equalsIgnoreCase("setprefix")) {
-                                c.setValue("prefix", value);
-                        } else if (action.equalsIgnoreCase("setsuffix")) {
-                                c.setValue("suffix", value);
-                        } else if (action.equalsIgnoreCase("setpriority")) {
-                                c.setValue("priority", value);
-                        }
-		}
-	}
+    private static WorldManager wm = WorldManager.getInstance();
 
+    public static void execute(String name, CalculableType type, String action, String value, String world) {
+        Set<World> worlds = new HashSet<World>();
+        // add all if null
+        if (world == null) {
+            worlds.addAll(wm.getAllWorlds());
+        } else {
+            worlds.add(wm.getWorld(world));
+        }
+        for (World w : worlds) {
+            Calculable c = w.get(name, type);
+            if (action.equalsIgnoreCase("addgroup")) {
+                c.addGroup(value);
+            } else if (action.equalsIgnoreCase("rmgroup")) {
+                c.removeGroup(value);
+            } else if (action.equalsIgnoreCase("setgroup")) {
+                for (String g : new ArrayList<String>(c.getGroupsAsString())) {
+                    c.removeGroup(g);
+                }
+                c.addGroup(value);
+            } else if (action.equalsIgnoreCase("addperm")) {
+                Permission perm = Permission.loadFromString(value);
+                c.addPermission(perm.nameLowerCase(), perm.isTrue());
+            } else if (action.equalsIgnoreCase("rmperm")) {
+                c.removePermission(value);
+            } else if (action.equalsIgnoreCase("setprefix")) {
+                c.setValue("prefix", value);
+            } else if (action.equalsIgnoreCase("setsuffix")) {
+                c.setValue("suffix", value);
+            } else if (action.equalsIgnoreCase("setpriority")) {
+                c.setValue("priority", value);
+            }
+        }
+    }
 }
