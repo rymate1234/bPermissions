a15162dfe24c9483a0b398d20823d9f294b43fd3
Ryan Murthick <supermarioryan@gmail.com>
2016-08-28 17:50:28 +0100

    [Sponge] Recalculate perms when changing world

--- sponge/src/main/java/de/bananaco/bpermissions/imp/PlayerHandler.java
+++ sponge/src/main/java/de/bananaco/bpermissions/imp/PlayerHandler.java
@@ -7,6 +7,7 @@ import de.bananaco.bpermissions.util.loadmanager.MainThread;
 import de.bananaco.bpermissions.util.loadmanager.TaskRunnable;
 import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.event.Listener;
+import org.spongepowered.api.event.entity.DisplaceEntityEvent;
 import org.spongepowered.api.event.network.ClientConnectionEvent;
 import org.spongepowered.api.service.context.Context;
 import org.spongepowered.api.service.permission.Subject;
@@ -59,22 +60,36 @@ public class PlayerHandler {
         setupPlayer(player, true);
     }
 
+    @Listener
+    public void playerUpdater(DisplaceEntityEvent.Teleport event) {
+        if (!(event.getTargetEntity() instanceof Player)) return;
+
+        if (!event.getFromTransform().getExtent().equals(event.getToTransform().getExtent())) {
+            Player player = (Player) event.getTargetEntity();
+            setupPlayer(player, event.getToTransform().getExtent().getName(), true);
+        }
+    }
+
     public void setupPlayer(Player player, boolean recalculate) {
         if (!permissions.getGame().getPluginManager().isLoaded("de.bananaco.bpermissions")) {
             return;
         }
 
+        setupPlayer(player, player.getWorld().getName(), recalculate);
+    }
+
+    public void setupPlayer(Player player, String world, boolean recalculate) {
         // Grab the pre-calculated effectivePermissions from the User object
         // Then whack it onto the player
         Map<String, Boolean> perms = ApiLayer.getEffectivePermissions(
-                player.getWorld().getName(),
+                world,
                 CalculableType.USER,
                 player.getUniqueId().toString(),
                 recalculate
         );
 
         // lets give this some context
-        Context worldContext = new Context(Context.WORLD_KEY, player.getWorld().getName());
+        Context worldContext = new Context(Context.WORLD_KEY, world);
         Set<Context> contexts = new HashSet<>();
         //contexts.add(worldContext);
 
@@ -83,7 +98,7 @@ public class PlayerHandler {
 
         if (subject instanceof bPermissionsSubjectData) {
             bPermissionsSubjectData bPermsSubject = ((bPermissionsSubjectData) subject);
-            bPermsSubject.setWorld(player.getWorld().getName());
+            bPermsSubject.setWorld(world);
         }
 
         if (perms.keySet().size() > 0) {
